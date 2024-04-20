ba8f97207fb3898c0ad6c54cffa327bb5a84fde2
Duarte007 <matheusdcarvalho007@gmail.com>
2024-04-20 20:04:24 -0300

    remove comment

--- routes.js
+++ routes.js
@@ -39,7 +39,6 @@ async function routes(fastify, options) {
   };
 
   fastify.post("/animals", { schema }, async (request, reply) => {
-    // we can use the `request.body` object to get the data sent by the client
     const result = await collection.insertOne({ animal: request.body.animal });
     return result;
   });
